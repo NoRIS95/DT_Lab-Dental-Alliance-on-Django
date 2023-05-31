9c01c3cce31cca4f8044fc75873849dce5b03713
NoRIS95 <ryzhikov95@gmail.com>
2023-05-31 21:24:27 +0300

    Sec_com

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('price', views.price_info, name="price"),
-    path("about-us", views.about, name="about")
+    path('price/', views.price_info, name="price"),
+    path("about-us/", views.about, name="about")
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
