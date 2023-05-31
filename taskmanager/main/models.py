c0cbafd8e8753657ac167e4cb0bab57d4ccd7e08
NoRIS95 <ryzhikov95@gmail.com>
2023-05-31 15:11:58 +0300

    Sec_com

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Task(models.Model):
     price = models.CharField("Цена вида протеза", max_length=7)
     name = models.TextField("Описание")
+    image = models.ImageField(null=True)
+
+    #Наша зуботехническая лаборатория занимается изготовлением разных зуботехнических изделий c 2012 года.Находимся по адресу г.Новосибирск, ул.Волховская 33\1. По условиям подробнее звоните по телефону и указанной почте 8-9180-423-50-44.
 
 # Create your models here.
     def __str__(self):
@@ -12,3 +15,15 @@ class Task(models.Model):
     class Meta:
         verbose_name = "Изделия"
         verbose_name_plural = "Изделия"
+
+class About(models.Model):
+    greeting = models.TextField("Приветствие")
+    email = models.CharField("E-mail", max_length=50)
+    tel = models.CharField("Телефон",  max_length=15)
+    image = models.ImageField(null=True)
+    def __str__(self):
+        return self.greeting
+
+    class Meta:
+        verbose_name = "Приветствие"
+        verbose_name_plural = "Приветствие"
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Task
+from .models import Task, About
 # Create your views here.
 
 
 
 def price_info(request):
     tasks = Task.objects.all()
-    return render(request, "main/price_info.html", {'title': "Цены на виды протезирования", "tasks": tasks})
+    return render(request, "main/price_info.html", {'title': "Цены на виды протезирования", "tasks": tasks,})
 
 
 def about(request):
-    return render(request, "main/about.html")
+    tasks = About.objects.all()
+    return render(request, "main/about.html",  {'title': "Приветствие", "tasks": tasks})
