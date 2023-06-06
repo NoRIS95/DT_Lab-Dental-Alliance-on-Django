67f9e10b985444ba972a77b7607389642fdda05a
NoRIS95 <ryzhikov95@gmail.com>
2023-06-06 19:20:38 +0300

    Final_commit

--- taskmanager/proj/settings.py
+++ taskmanager/proj/settings.py
@@ -120,8 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
-#
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
